092daff2cc362358186f356905477fafdad682ca
Ari Susanto <risusanto@outlook.com>
2018-03-14 10:25:37 +0700

    initial commit

---
+++ friend.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+class A{
+    private:
+        int num;
+    public:
+        A(): num(6) {}
+        friend int addNum(A);
+};
+
+int addNum(A numA){
+    return numA.num+5;
+}
+
+using namespace std;
+int main(){
+    A myA;
+    myA.num = 7;
+    cout<<"A: " << addNum(myA) << endl;
+    return 0;
+}
